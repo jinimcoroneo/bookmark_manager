2c718e6504ff75b966cbf4d6f39f44587e7d74a9
jinimcoroneo <jinim1983@gmail.com>
2017-06-30 16:31:58 +0100

    fix errors

--- app/init.rb
+++ app/init.rb
@@ -7,4 +7,6 @@ require "./app/models/user.rb"
 
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+#DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_test")
+#DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_development")
 DataMapper.finalize
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,23 +42,23 @@ RSpec.configure do |config|
     # ...rather than:
     #     # => "be bigger than 2"
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-    
+
     config.after(:suite) do
       DatabaseCleaner.clean
     end
-    
+
     config.before(:each) do
       DatabaseCleaner.clean_with(:truncation)
     end
-    
+
     config.before(:each) do
       DatabaseCleaner.strategy = :transaction
     end
-    
+
     config.before(:each) do
       DatabaseCleaner.start
     end
-    
+
     # rspec-mocks config goes here. You can use an alternate test double
     # library (such as bogus or mocha) by changing the `mock_with` option here.
     config.mock_with :rspec do |mocks|
@@ -67,14 +67,14 @@ RSpec.configure do |config|
       # `true` in RSpec 4.
       mocks.verify_partial_doubles = true
     end
-    
+
     # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
     # have no way to turn it off -- the option exists only for backwards
     # compatibility in RSpec 3). It causes shared context metadata to be
     # inherited by the metadata hash of host groups and examples, rather than
     # triggering implicit auto-inclusion in groups with matching metadata.
     config.shared_context_metadata_behavior = :apply_to_host_groups
-    
+
     # The settings below are suggested to provide a good initial experience
     # with RSpec, but feel free to customize to your heart's content.
     # =begin
